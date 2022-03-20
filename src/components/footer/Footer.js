55862eef8533f3902875e209f5bcac9bdeb4a8ae
Kirti Sharma <kirti2001sharma@gmail.com>
2022-03-21 01:27:07 +0530

    feat:added main and footer section to homepage

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer class="footer flex">
+      <small>
+        Made with ❤ by <strong class="imp-data">Kirti Sharma </strong>
+      </small>
+
+      <div class="footer-links flex">
+        <a href="https://github.com/kirti2602">
+          <i class="fab fa-github"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/kirti--sharma/">
+          <i class="fab fa-linkedin"></i>
+        </a>
+        <a href="https://twitter.com/_keeerti">
+          <i class="fab fa-twitter"></i>
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
