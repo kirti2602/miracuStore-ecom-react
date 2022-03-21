33727836c7f62a9a515426906a760a6a47342cb1
Kirti Sharma <kirti2001sharma@gmail.com>
2022-03-21 17:27:43 +0530

    feat: added products and homepage router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import HomePage from "./pages/homepage/HomePage";
+import ProductListing from "./pages/productListing/ProductListing";
 
 function App() {
   return (
     <div className="">
       <HomePage className= "homepage"/>
+      <ProductListing />
     </div>
   );
 }
