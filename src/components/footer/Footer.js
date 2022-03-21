5f0c3de5734143cd15a90cdd6eae042776582c15
Kirti Sharma <kirti2001sharma@gmail.com>
2022-03-21 12:05:44 +0530

    fix:changed class to className

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,20 +2,20 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class="footer flex">
+    <footer className="footer flex">
       <small>
-        Made with ❤ by <strong class="imp-data">Kirti Sharma </strong>
+        Made with ❤ by <strong className="imp-data">Kirti Sharma </strong>
       </small>
 
-      <div class="footer-links flex">
+      <div className="footer-links flex">
         <a href="https://github.com/kirti2602">
-          <i class="fab fa-github"></i>
+          <i className="fab fa-github"></i>
         </a>
         <a href="https://www.linkedin.com/in/kirti--sharma/">
-          <i class="fab fa-linkedin"></i>
+          <i className="fab fa-linkedin"></i>
         </a>
         <a href="https://twitter.com/_keeerti">
-          <i class="fab fa-twitter"></i>
+          <i className="fab fa-twitter"></i>
         </a>
       </div>
     </footer>
